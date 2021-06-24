65efe9feb2d2317e7b9a62a977d9f26f7eafc461
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-24 15:28:09 +0530

    empwageuc2

--- src/empwagebuilder/Empwagebuilder.java
+++ src/empwagebuilder/Empwagebuilder.java
@@ -2,17 +2,25 @@ package empwagebuilder;
 
 public class Empwagebuilder {
 	public static void main(String args[]) { 
-		int FULLDAY = 8;
+		int FULL_DAY = 8;
+		int PART_TIME = 4;
 		int WAGE_PER_HOUR =20;
                 double IS_EMP_PRESENT = 1.0;
-				double empCheck = Math.floor(Math.random() *10) % 2;
+                double IS_PART_TIME = 2.0;
+				double empCheck = Math.floor(Math.random() *10) % 3;
 				if (empCheck == IS_EMP_PRESENT) {
-					int dailywage = FULLDAY *WAGE_PER_HOUR;
+					int dailywage = FULL_DAY *WAGE_PER_HOUR;
 					System.out.println("Empoyee is Present");
 					System.out.println("Daily Employee Wage is : "+ dailywage);
-				}else {
-					System.out.println("Employee is Abscent");
+				}
+				else if(empCheck  == IS_PART_TIME) {
+					int PartTimeWage = PART_TIME *WAGE_PER_HOUR;
+					System.out.println("Employee is Present for part time");
+					System.out.println("Daily Employee Wageis : "+ PartTimeWage);
 				} 
+				else {
+					System.out.println("Employee is absent");
+				}
 	  } 
 
 }
