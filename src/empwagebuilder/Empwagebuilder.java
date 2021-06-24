cb13061617a5869b18ed8bc395c13cc3af47ee2f
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-24 14:54:28 +0530

    Descripition

--- src/empwagebuilder/Empwagebuilder.java
+++ src/empwagebuilder/Empwagebuilder.java
@@ -1,17 +1,18 @@
 package empwagebuilder;
 
 public class Empwagebuilder {
-
-	public static void main(String[] args) {
-		// constants
-				int IS_FULL_TIME = 1;
-				       // computation
+	public static void main(String args[]) { 
+		int FULLDAY = 8;
+		int WAGE_PER_HOUR =20;
+                double IS_EMP_PRESENT = 1.0;
 				double empCheck = Math.floor(Math.random() *10) % 2;
-				if (empCheck == IS_FULL_TIME)
+				if (empCheck == IS_EMP_PRESENT) {
+					int dailywage = FULLDAY *WAGE_PER_HOUR;
 					System.out.println("Empoyee is Present");
-					else
-						System.out.println("Employee is Abscent");
-
-	}
+					System.out.println("Daily Employee Wage is : "+ dailywage);
+				}else {
+					System.out.println("Employee is Abscent");
+				} 
+	  } 
 
 }
