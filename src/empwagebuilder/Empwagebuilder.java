662ad12a115421fc6ef315138d7142e8be5fd5eb
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-24 14:42:32 +0530

    EmpWageBuilder

---
+++ src/empwagebuilder/Empwagebuilder.java
@@ -0,0 +1,17 @@
+package empwagebuilder;
+
+public class Empwagebuilder {
+
+	public static void main(String[] args) {
+		// constants
+				int IS_FULL_TIME = 1;
+				       // computation
+				double empCheck = Math.floor(Math.random() *10) % 2;
+				if (empCheck == IS_FULL_TIME)
+					System.out.println("Empoyee is Present");
+					else
+						System.out.println("Employee is Abscent");
+
+	}
+
+}
